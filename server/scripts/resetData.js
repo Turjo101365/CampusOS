b56fdbb7b33eaeaae79ebf7ea687d59431a8c1cc
Prottoy Saha Dip <prottoysahadip2@gmail.com.com>
2026-09-04 18:24:28 +0600

    Clean Architecture

--- server/scripts/resetData.js
+++
@@ -1,7 +0,0 @@
-import { initStorage, resetToSeed } from '../db/storage.js';
-
-console.log('Resetting CampusOS database to initial seed data...');
-initStorage();
-const result = resetToSeed();
-console.log(result.message);
-process.exit(0);
